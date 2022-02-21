75006822c83a810ddb00ccfe63ad1cd92ff33de3
joshrogers857 <87278992+joshrogers857@users.noreply.github.com>
2022-02-21 20:00:35 +0000

    Add hide keyboard

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     @State private var chequeAmount = 0.0
     @State private var numberOfPeople = 0
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
@@ -37,6 +38,7 @@ struct ContentView: View {
                               format: .currency(
                                 code: Locale.current.currencyCode ?? "GBP"))
                         .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
                     
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) {
@@ -64,6 +66,15 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
