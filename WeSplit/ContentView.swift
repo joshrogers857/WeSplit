ce5c52bf9051fd2cfd58520c8981926e334e7509
joshrogers857 <87278992+joshrogers857@users.noreply.github.com>
2022-02-21 23:46:56 +0000

    Wrap preview in Stack to fix @FocusState issue

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -91,6 +91,8 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ZStack {
+            ContentView()
+        }
     }
 }
